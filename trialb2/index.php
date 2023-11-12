466112eeaf72c8e2094756bcbc22400e6b4ee270
Esedo Fredrick <50068759+areaboy@users.noreply.github.com>
2023-11-12 16:49:03 +0100

    Add files via upload

---
+++ trialb2/index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$_SESSION['px'] = '200';
+
+
+?>
+<a href='index1.php'>Db connect</a><br><br>
+<a href='index2.php'>session</a><br><br>
+
+
---
+++ trialb2/index1.php
@@ -0,0 +1,57 @@
+<?php
+//error_reporting(0); 
+$openshift_mysql_db_servername = "172.30.22.187:3306";
+$openshift_mysql_db_username = "userKBW";
+$openshift_mysql_db_password = "csSpMlS8ArEjWfOq";
+$openshift_mysql_db_port = 3306;
+$openshift_mysql_db_name ="sampledb";
+$tidb_options = array(
+	//PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	PDO::MYSQL_ATTR_SSL_CA => 'cacert-2023-05-30.pem',
+	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => 'VERIFY_IDENTITY',
+);
+try {
+
+$db = new PDO("mysql:host=$openshift_mysql_db_servername;dbname=$openshift_mysql_db_name;charset=utf8", $openshift_mysql_db_username, $openshift_mysql_db_password);
+
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ echo "Openshift Mysql Database Connected successfully...."; 
+}
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+echo "<div style='color:white;background:red;padding:10px;border:none;'>Connection to Openshift Mysql Cloud Database Failed...Check your Mysql Database Credentials and Internet as well</div>";
+}
+
+
+
+/*
+
+//error_reporting(0); 
+$openshift_mysql_db_servername = "localhost:3306";
+$openshift_mysql_db_username = "root";
+$openshift_mysql_db_password = "";
+$openshift_mysql_db_port = 3306;
+$openshift_mysql_db_name ="sampledb";
+$tidb_options = array(
+	//PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	PDO::MYSQL_ATTR_SSL_CA => 'cacert-2023-05-30.pem',
+	PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => 'VERIFY_IDENTITY',
+);
+try {
+
+$db = new PDO("mysql:host=$openshift_mysql_db_servername;dbname=$openshift_mysql_db_name;charset=utf8", $openshift_mysql_db_username, $openshift_mysql_db_password);
+
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ echo "Openshift Mysql Database Connected successfully  and working."; 
+}
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+echo "<div style='color:white;background:red;padding:10px;border:none;'>Connection to Openshift Mysql Cloud Database Failed...Check your Mysql Database Credentials and Internet as well</div>";
+}
+
+
+*/
